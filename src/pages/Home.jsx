e6cff87b1684131090624ecdba84c3179071a92a
renanmoraez <renanmoraesaraujo@gmail.com>
2022-07-15 00:02:35 -0300

    primeiro commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+// css
+import "./MovieGrid.css"
+
+const Home = () => {
+
+    const [topMovies, setTopMoveis] = useState([])
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json();
+        setTopMoveis(data.results)
+    }
+
+    useEffect(() => {
+            const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+            getTopRatedMovies(topRatedUrl)
+    }, [])
+
+  return (
+    <div className="container">
+        <h2 className="title">Melhores Filmes: 🔥</h2>
+        <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+        </div>
+    </div>
+  )
+}
+
+export default Home
